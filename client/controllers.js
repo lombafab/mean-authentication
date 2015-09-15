2741a9110b7035d4669ac65286448e0d5e5e9ff6
Fabio <fabio.lombardi@bonitasoft.com>
2015-09-15 17:49:25 +0200

    Added Login controller and page

---
+++ client/controllers.js
@@ -0,0 +1,28 @@
+/**
+ * Created by fabiolombardi on 15/09/2015.
+ */
+angular.module('myApp').controller('loginController', ['$scope', '$location', 'AuthService', function($scope, $location, AuthService){
+
+    console.log(AuthService.getUserStatus());
+
+    $scope.login = function() {
+        // initial values
+        $scope.error = false;
+        $scope.disabled = true;
+
+        // call login form service
+        AuthService.login($scope.loginForm.username, $scope.loginForm.password)
+            //handle success
+            .then(function() {
+                $location.path('/');
+                $scope.disabled = false;
+                $scope.loginForm = {}
+            })
+
+            // handle error
+            .catch(function() {
+                $scope.error = true;
+                $scope.errorMessage = "Invalid username and/or password";
+            })
+    }
+}]);
