472662138c14fe8a9f97fcc8e36f59898a24c69b
Fabio <fabio.lombardi@bonitasoft.com>
2015-09-16 16:24:44 +0200

    Added register and retricted access to page

--- client/controllers.js
+++ client/controllers.js
@@ -1,7 +1,9 @@
 /**
  * Created by fabiolombardi on 15/09/2015.
  */
-angular.module('myApp').controller('loginCtrl', ['$scope', '$location', 'AuthService', function($scope, $location, AuthService){
+angular.module('myApp')
+
+.controller('loginCtrl', ['$scope', '$location', 'AuthService', function($scope, $location, AuthService){
 
     console.log(AuthService.getUserStatus());
 
@@ -15,7 +17,7 @@ angular.module('myApp').controller('loginCtrl', ['$scope', '$location', 'AuthSer
             //handle success
             .then(function() {
                 $location.path('/');
-                $scope.disabled = false;
+                $scope.disabled = true;
                 $scope.loginForm = {}
             })
 
@@ -23,6 +25,45 @@ angular.module('myApp').controller('loginCtrl', ['$scope', '$location', 'AuthSer
             .catch(function() {
                 $scope.error = true;
                 $scope.errorMessage = "Invalid username and/or password";
+            })
+    };
+}])
+
+.controller('logoutCtrl', ['$scope', '$location', 'AuthService', function($scope, $location, AuthService){
+    $scope.logout = function(){
+        console.log(AuthService.getUserStatus());
+
+        // call logout from service
+        AuthService.logout()
+            .then(function(){
+                $location.path('/login');
+            })
+    };
+}])
+
+.controller('registerCtrl', ['$scope', '$location', 'AuthService', function($scope, $location, AuthService){
+    console.log(AuthService.getUserStatus());
+
+    $scope.register = function() {
+        // initial values
+        $scope.error = false;
+        $scope.disabled = false;
+
+        // call register from service
+        AuthService.register($scope.registerForm.username, $scope.registerForm.password)
+            //handle success
+            .then(function(){
+                $location.path('/login')
+                $scope.disabled = false;
+                $scope.registerForm = {}
+            })
+
+            // handle error
+            .catch(function(){
+                $scope.error = true;
+                $scope.errorMessage = "User already exists"
+                $scope.disabled = false;
+                $scope.registerForm = {};
             })
     };
 }]);
--- client/main.js
+++ client/main.js
@@ -1,12 +1,40 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.config(function ($routeProvider) {
+myApp
+
+.config(function ($routeProvider) {
   $routeProvider
     .when('/', {templateUrl: '/partials/home.html'})
-    .when('/login', {templateUrl: 'partials/login.html', controller:'loginCtrl'})
-    .when('/register', {templateUrl: 'partials/register.html', controller:'registerCtrl'})
-    .when('/logout', {controller:'logoutCtrl'})
-    .when('/one', {template: '<h1>This is page one!</h1>'})
-    .when('/two', {template: '<h1>This is page two!</h1>'})
-    .otherwise({redirectTo: '/'});
+    .when('/login', {
+          templateUrl: 'partials/login.html',
+          controller:'loginCtrl',
+          access: {restricted: false}
+    })
+    .when('/register', {
+          templateUrl: 'partials/register.html',
+          controller:'registerCtrl',
+          access: {restricted: false}
+    })
+    .when('/logout', {
+          controller:'logoutCtrl',
+          access: {restricted: true}
+    })
+    .when('/one', {
+          template: '<h1>This is a restricted page!</h1>',
+          access: {restricted: true}
+    })
+    .when('/two', {
+          template: '<h1>This is page two!</h1>',
+          access: {restricted: false}
+    })
+    .otherwise({redirectTo: '/'})
+})
+
+.run(function($rootScope, $location, $route, AuthService){
+    $rootScope.$on('$routeChangeStart', function(event, next, current) {
+        if (next.access.restricted && AuthService.isLoggedIn() === false) {
+            $location.path('login');
+            $route.reload();
+        }
+    })
 });
